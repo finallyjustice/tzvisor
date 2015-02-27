a4d496968ab54a92539b900eeab6f225bfae38b1
finallyjustice <dongli.zhang0129@gmail.com>
2015-02-27 14:59:41 -0500

    add clean linux kernel

--- kvm/kvm-dev/kvm-dev.c
+++ kvm/kvm-dev/kvm-dev.c
@@ -8,21 +8,25 @@ long sched_penalty = 100 * HZ / 1000;
 	
 void tzvisor_hypercall_handler(void)
 {
-	printk(KERN_ALERT "[VMM-Mod] tzvisor_hypercall_handler\n");
-	
+	printk(KERN_ALERT "[kvm-dev] tzvisor_hypercall_handler\n");
+
+	asm volatile(
+			".arch_extension sec\n\t"
+			"smc #0\n\t");
+
 	current->state = TASK_UNINTERRUPTIBLE;
 	schedule_timeout(sched_penalty);
 }
 
 static int __init kvm_dev_init(void)
 {
-	printk(KERN_ALERT "[VMM-Mod] init the module\n");
+	printk(KERN_ALERT "[kvm-dev] init the module\n");
 	tzvisor_hypercall_addr = (unsigned int)tzvisor_hypercall_handler;
 	return 0;
 }
 static void __exit kvm_dev_exit(void)
 {
-	printk(KERN_ALERT "[VMM-Mod] exit the module\n");
+	printk(KERN_ALERT "[kvm-dev] exit the module\n");
 }
 
 module_init(kvm_dev_init);
