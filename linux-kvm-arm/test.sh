1e064c97b4642c70ee459780a0f422925c336be1
finallyjustice <dongli.zhang0129@gmail.com>
2015-04-20 12:30:09 -0400

    change compiler for kernel

--- linux-kvm-arm/test.sh
+++ linux-kvm-arm/test.sh
@@ -1,1 +1,4 @@
-LOADADDR=0x80008000 CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm make uImage
+# dd if=uImage of=zImage skip=64 bs=1
+
+#LOADADDR=0x80008000 CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm make uImage
+LOADADDR=0x80008000 CROSS_COMPILE=arm-none-eabi- ARCH=arm make uImage
